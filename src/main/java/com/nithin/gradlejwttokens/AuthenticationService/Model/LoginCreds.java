b71665e787ed1d95957bb327399eb6c63c31122b
nithinsoundar <nithinsoundar@gmail.com>
2023-02-22 10:26:27 +0530

    Commit and push

--- src/main/java/com/nithin/gradlejwttokens/AuthenticationService/Model/LoginCreds.java
+++ src/main/java/com/nithin/gradlejwttokens/AuthenticationService/Model/LoginCreds.java
@@ -1,11 +1,13 @@
 package com.nithin.gradlejwttokens.AuthenticationService.Model;
 
 import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Data
 public class LoginCreds {
     private String username;
     private String password;
+    @Field("role")
     private Role role;
 
 
--- src/main/java/com/nithin/gradlejwttokens/AuthenticationService/Model/User.java
+++ src/main/java/com/nithin/gradlejwttokens/AuthenticationService/Model/User.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 
 @Data
@@ -20,5 +21,7 @@ public class User {
     private String username;
     private String email;
     private String password;
+
+    @Field("role")
     private Role role;
 }
